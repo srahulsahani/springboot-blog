06725e4d7ac35ad6bf54177b86bf5f37021435fa
Rahul Sahani <srahulsahani@gmail.com>
2023-07-09 23:19:09 +0530

    Paging and Sorting

--- src/main/java/com/springboot/blog/controller/PostController.java
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -2,7 +2,9 @@ package com.springboot.blog.controller;
 
 
 import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.payload.PostResponse;
 import com.springboot.blog.service.PostService;
+import com.springboot.blog.utils.AppConstants;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,9 +26,16 @@ public class PostController {
         return  new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
     }
 
+    /* GET all POST*/
     @GetMapping
-    List<PostDto> getAllPost(){
-        return postService.getAllPost();
+    PostResponse getAllPost(
+            @RequestParam( value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIRECTION, required = false) String sortDir
+
+    ){
+        return postService.getAllPost(pageNo,pageSize,sortBy,sortDir);
     };
 
     /* Search the post by id */
